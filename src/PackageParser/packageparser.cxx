c8004de69efa91ede64f0542703fe03ea8b2c67a
spirited-casey <dmitry.leshchikov@yandex.ru>
2025-06-21 14:46:08 +0300

    changed parsing method from reading multiple times and parse to read once and parse

--- src/PackageParser/packageparser.cxx
+++ src/PackageParser/packageparser.cxx
@@ -17,6 +17,27 @@ namespace
 			((addr << 16) >> 24),
 			((addr << 24) >> 24));
 	}
+
+	void endianness(uint8_t* data, size_t size)
+	{
+		if constexpr (std::endian::native == std::endian::big)
+		{
+			return;
+		}
+
+		--size;
+
+		size_t start = 0;
+		
+		while (start < size)
+		{
+			std::swap(data[start++], data[size--]);
+		}
+
+		uint32_t tmp = *reinterpret_cast<uint32_t*>(data);
+
+		int a = 1;
+	}
 }
 
 namespace train_protocol
@@ -30,78 +51,65 @@ namespace train_protocol
 			throw std::system_error(errno, std::system_category(), "Error: failed to open file.");
 		}
 
-		std::array<int8_t, 2> pkg_size {};
-		uint16_t size;
+		std::array<uint8_t, 2> pkg_size {};
+		uint16_t size {};
 
-		while (file.read(reinterpret_cast<char*>(pkg_size.data()), 2))
+		while (!file.eof())
 		{
 			++_total_packets_processed;
 
-			if constexpr (std::endian::native == std::endian::big)
-			{
-				size = static_cast<uint16_t>(
-					pkg_size[1] |
-					(pkg_size[0] << 8));
-			}
-			else
-			{
-				size = static_cast<uint16_t>(
-					pkg_size[0] |
-					(pkg_size[1] << 8));
-			}
+			file.read(reinterpret_cast<char*>(pkg_size.data()), 2);
+			size = pkg_size[0] | (pkg_size[1] << 8);
 
-			// valid packets size only
-			if (size >= 64 && size <= 1522)
 			{
-				boost::asio::post(_tp, [this, &_path, pos = file.tellg()]()
+				std::vector<uint8_t> payload;
+
+				payload.resize(size);
+				file.read(reinterpret_cast<char*>(payload.data()), size);
+
+				boost::asio::post(_tp, [this, data = std::move(payload)]()
 					{
-						std::ifstream file{ _path, std::ios::in | std::ios::binary };
-
-						file.seekg(pos);
-						this->worker(std::move(file));
+						this->worker(std::move(data));
 					});
 			}
-
-			file.seekg(file.tellg() + static_cast<std::ifstream::pos_type>(size));
 		}
+
+		_tp.join();
 	}
 
-	void TrainPacketsParser::worker(std::ifstream pkg_file)
+	void TrainPacketsParser::worker(std::vector<uint8_t> payload)
 	{
-		auto convert = [](const std::array<uint8_t, 2>& field)
+		static constexpr size_t ETHERTYPE_START_POS = 12;
+		static constexpr size_t ADDRS_START_POS = 26;
+
+		auto convert = [](uint16_t field)
 			{
 				if constexpr (std::endian::native == std::endian::big)
 				{
-					return static_cast<uint16_t>(
-						field[0] |
-						(field[1] << 8));
+					return field;
 				}
-				else
-				{
-					return static_cast<uint16_t>(
-						field[1] |
-						(field[0] << 8));
-				}
+
+				return (field >> 8) | (field << 8);
 			};
-		std::array<uint8_t, 2> ether_type_field{};
-		uint16_t type;
 
-		// skip to tag or EtherType value
-		pkg_file.seekg(pkg_file.tellg() + static_cast<std::ifstream::pos_type>(12));
-		pkg_file.read(reinterpret_cast<char*>(ether_type_field.data()), 2);
+		uint16_t type {};
+		uint64_t src_dst_addrs {};
 
-		type = convert(ether_type_field);
+		std::memcpy(&type, &payload[ETHERTYPE_START_POS], sizeof(uint16_t));
+		type = convert(type);
 
 		// VLAN
 		if (type == 0x8100)
 		{
-			pkg_file.read(reinterpret_cast<char*>(ether_type_field.data()), 2);
-			type = convert(ether_type_field);
+			std::memcpy(&type, &payload[ETHERTYPE_START_POS + 2], sizeof(uint16_t));
+			std::memcpy(&src_dst_addrs, &payload[ADDRS_START_POS + 2], sizeof(uint64_t));
+			type = convert(type);
 		}
 		else if (type == 0x88A8)
 		{
-			pkg_file.read(reinterpret_cast<char*>(ether_type_field.data()), 2);
-			type = convert(ether_type_field);
+			std::memcpy(&type, &payload[ETHERTYPE_START_POS + 4], sizeof(uint16_t));
+			std::memcpy(&src_dst_addrs, &payload[ADDRS_START_POS + 4], sizeof(uint64_t));
+			type = convert(type);
 		}
 		if (type != 0x0800)
 		{
@@ -109,27 +117,13 @@ namespace train_protocol
 			return;
 		}
 
-		std::array<uint8_t, 4> src{}, dst{};
-
-		pkg_file.seekg(pkg_file.tellg() + static_cast<std::ifstream::pos_type>(12));
-		pkg_file.read(reinterpret_cast<char*>(src.data()), 4);
-		pkg_file.read(reinterpret_cast<char*>(dst.data()), 4);
-
-		uint32_t src_val = 
-			(src[0] << 24) |
-			(src[1] << 16) |
-			(src[2] << 8) |
-			(src[3]);
-		uint32_t dst_val =
-			(dst[0] << 24) |
-			(dst[1] << 16) |
-			(dst[2] << 8) |
-			(dst[3]);
+		std::memcpy(&src_dst_addrs, &payload[ADDRS_START_POS], sizeof(uint64_t));
+		endianness(reinterpret_cast<uint8_t*>(&src_dst_addrs), sizeof(uint64_t));
 
 		{
 			std::lock_guard<std::mutex> lock(_mut);
 
-			++_from_to_addrs_count_map[src_val][dst_val];
+			++_from_to_addrs_count[src_dst_addrs];
 		}
 	}
 
@@ -142,24 +136,25 @@ namespace train_protocol
 		os << std::setw(25) << std::left << "Packets without IPv4:" << obj._non_ipv4_packets << "\n\n";
 
 		std::unordered_map<uint32_t, std::string> _addr_to_str;
+		uint32_t from{}, to{};
 
-		for (const auto& [from, to_addrs] : obj._from_to_addrs_count_map)
+		for (const auto& [from_to_addrs, count] : obj._from_to_addrs_count)
 		{
+			from = from_to_addrs >> 32;
+			to = (from_to_addrs << 32) >> 32;
+
 			if (!_addr_to_str.contains(from))
 			{
 				_addr_to_str[from] = std::move(convert_addr_to_str(from));
 			}
-			for (const auto& [to, count] : to_addrs)
+			if (!_addr_to_str.contains(to))
 			{
-				if (!_addr_to_str.contains(to))
-				{
-					_addr_to_str[to] = std::move(convert_addr_to_str(to));
-				}
-
-				os << std::setw(15) << std::left << _addr_to_str[from] << "-> "
-					<< std::setw(15) << std::left << _addr_to_str[to]
-					<< " " << count << '\n';
+				_addr_to_str[to] = std::move(convert_addr_to_str(to));
 			}
+
+			os << std::setw(15) << std::left << _addr_to_str[from] << "-> "
+				<< std::setw(15) << std::left << _addr_to_str[to]
+				<< " " << count << '\n';
 		}
 
 		return os;
